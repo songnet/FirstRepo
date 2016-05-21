2056e93c2c5ad427121f5f168ab8fcc5ca823f2c
songpeng_homepc <www.15712847805@163.com>
2016-05-21 18:52:54 +0800

    2016年5月21日18:52:50

---
+++ demo/WebProgressBar/Handler1.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace WebProgressBar
+{
+    /// <summary>
+    /// Handler1 的摘要说明
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+
+            string data = "ajax返回result";
+            context.Response.ContentType = "text/plain";
+            //Thread.Sleep(5000);
+            context.Response.Write(data);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
